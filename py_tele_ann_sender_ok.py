4f00928d96bbb523eef97de164d3ea2250b2d6b9
jinjinpy <70568749+jinjinpy@users.noreply.github.com>
2020-09-01 18:56:02 +0900

    Update py_tele_ann_sender_ok.py

--- py_tele_ann_sender_ok.py
+++ py_tele_ann_sender_ok.py
@@ -1,26 +1,14 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import telepot
-#import os
 
-access_token = '1336751738:AAGYLFLDIzXARTkti1KuhmQPizrGDbph0oM'
-#os.environ["BOT_TOKEN"]
-mc = '1141765178'
-bot = telepot.Bot(token)
-
-def handle(msg):
-    with urlopen('https://www.jbnu.ac.kr/kor/?menuID=139&pno=1') as response:
-        soup = BeautifulSoup(response, 'html.parser')
-        i = 1
-        for anchor in soup.select('a[href^="?menuID=139&pno=1&mode=view&no="]'):
-            announcement = anchor.get_text(strip=True)
-            if not announcement.find(msg['text']) == -1:
-                data = '[' + str(i) + '] ' + announcement
-                print(data)
-                bot = telepot.Bot(access_token)
-                bot.sendMessage(msg['from']['id'], data)
-                i = i + 1
-bot.message_loop(handle)
-
-while True:
-    pass
+with urlopen('https://www.jbnu.ac.kr/kor/?menuID=139&pno=1') as response:
+    soup = BeautifulSoup(response, 'html.parser')
+    i = 1
+    for anchor in soup.select('a[href^="?menuID=139&pno=1&mode=view&no="]'):
+        announcement = anchor.get_text(strip=True)
+        data = '[' + str(i) + '] ' + announcement
+        print(data)
+        bot = telepot.Bot('1336751738:AAGYLFLDIzXARTkti1KuhmQPizrGDbph0oM')
+        bot.sendMessage(1141765178, data)
+        i += 1
